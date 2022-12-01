9416e8eb2f969f1c314de96952f497398777d3ee
gofrendi <gofrendiasgard@gmail.com>
2022-12-01 20:41:38 +0700

    modify .ini

---
+++ generate.sh
@@ -0,0 +1,2 @@
+alembic --name one revision -m "new rev for schema 2" --autogenerate
+alembic --name two revision -m "new rev for schema 2" --autogenerate
--- migrate.sh
+++
@@ -1,2 +0,0 @@
-alembic --name schema1 revision -m "new rev for schema 2" --autogenerate
-alembic --name schema2 revision -m "new rev for schema 2" --autogenerate
