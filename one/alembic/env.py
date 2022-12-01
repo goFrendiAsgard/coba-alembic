4ac4b572920783c607510419d664b823672cb0c6
gofrendi <gofrendiasgard@gmail.com>
2022-12-01 20:53:03 +0700

    import base

--- one/alembic/env.py
+++ one/alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from one.model.book import Book
+from one.model.book import Book, Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
--- two/alembic/env.py
+++ two/alembic/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from two.model.user import User
+from two.model.user import User, Base
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
