766b87df39fea4b1b91ffcc62ab63ded0ea6bd20
gofrendi <gofrendiasgard@gmail.com>
2022-12-01 21:40:32 +0700

    update migration name

--- generate.sh
+++ generate.sh
@@ -1,2 +1,2 @@
-alembic --name one revision -m "new rev for schema 2" --autogenerate
+alembic --name one revision -m "new rev for schema 1" --autogenerate
 alembic --name two revision -m "new rev for schema 2" --autogenerate
