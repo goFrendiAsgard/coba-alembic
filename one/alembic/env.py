cd98e821dd6f691238d6b46f013d2a6d340d31df
gofrendi <gofrendiasgard@gmail.com>
2022-12-01 20:57:35 +0700

    fix auto-generate

--- one/alembic/env.py
+++ one/alembic/env.py
@@ -5,7 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from one.model.book import Book, Base
+from one.model.book import Base
+from one.model.book import Book
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -20,7 +21,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = Base.metadata
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
